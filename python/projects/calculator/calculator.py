d0a9fa1c52c318c46f50f8cd8db3c51e53efb5b9
Sandra-Ayman <sandraayman52@gmail.com>
2022-11-05 22:39:49 +0200

    this project for calculator in programmer mode and standard mode

--- python/projects/calculator/calculator.py
+++ python/projects/calculator/calculator.py
@@ -1,10 +1,9 @@
-from pickle import FALSE, TRUE
-
-
+#This project for a calculator in programmer mode and standard mode
 print("For programmer mode press 1")
 print("For standard mode press 2")
 press=int(input("Enter your choice:"))
 out=True
+#if press==1 this means we are in a programmer mode and every number prints in binary
 if(press==1):
     number_1 = int(input("Enter the first number:"))
     number_2 = int(input("Enter the second number:"))
@@ -28,9 +27,11 @@ if(press==1):
             result = number_1 * number_2
             print(bin(number_1)," * ",bin(number_2)," = ",bin(result))
         elif press==4 :
+            #check it gets infinity number or not
             if number_2==0:
                 print("can not do this operation")
             else:
+                #casting here because the binary can't print as a float number
                 result = int(number_1 / number_2)
                 print(bin(number_1)," / ",bin(number_2)," = ",bin(result))
         elif press==5:
@@ -43,6 +44,7 @@ if(press==1):
             result = number_1 ^ number_2
             print(bin(number_1)," ^ ",bin(number_2)," = ",bin(result))
         elif press==8:
+            #check which number to complement
             print("Which number do you want to complement (1: first number,2: second number):")
             press=int(input("Enter your choice:"))
             if press==1:
@@ -55,6 +57,7 @@ if(press==1):
                 print("Wrong choice!")
         else:
             print("Wrong choice!\n","please try again\n")
+        #check the user need another opertion or not
         print("Do you want anything else(please enter 1 for yes and 2 for no):")
         press=int(input())
         if press==1:
@@ -64,6 +67,7 @@ if(press==1):
         else:
             print("Wrong choice!\n","please try again later\n")
             out=False
+#if press==2 this means we are in a standard mode
 elif press==2:
     number_1 = int(input("Enter the first number:"))
     number_2 = int(input("Enter the second number:"))
@@ -83,6 +87,7 @@ elif press==2:
             result = number_1 * number_2
             print(number_1," * ",number_2," = ",result)
         elif press==4 :
+            #check it gets infinity number or not
             if number_2==0:
                 print("can not do this operation")
             else:
@@ -90,6 +95,7 @@ elif press==2:
                 print(number_1," / ",number_2," = ",result)
         else:
             print("Wrong choice!\n","please try again\n")
+        #casting here because the binary can't print as a float number
         print("Do you want anything else(please enter 1 for yes and 2 for no):")
         press=int(input())
         if press==1:
